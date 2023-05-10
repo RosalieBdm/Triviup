b0dea59600c3c61e532d2589c78ffccad7c4b0ea
unknown <augereau.jeremy@protonmail.com>
2023-05-10 19:35:49 +0200

    post score ok

--- app/src/main/java/com/example/triviup/ResultFragment.kt
+++ app/src/main/java/com/example/triviup/ResultFragment.kt
@@ -1,10 +1,15 @@
 package com.example.triviup
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import androidx.navigation.fragment.findNavController
 import com.example.triviup.databinding.FragmentResultBinding
 import com.example.triviup.model.Rank
 import com.google.firebase.database.DatabaseReference
@@ -14,6 +19,7 @@ import nl.dionsegijn.konfetti.core.Party
 import nl.dionsegijn.konfetti.core.Position
 import nl.dionsegijn.konfetti.core.emitter.Emitter
 import java.util.concurrent.TimeUnit
+import kotlin.random.Random
 
 
 class ResultFragment : Fragment() {
@@ -41,14 +47,34 @@ class ResultFragment : Fragment() {
             emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(100),
             position = Position.Relative(0.5, 0.15)
         )
-
         binding.submitButton.setOnClickListener {
-            binding.konfettiView.start(party)
+            sendScore(binding.edittext.text.toString(), Random.nextInt(0,501))
+            findNavController().navigate(R.id.action_resultFragment_to_FirstFragment)
         }
+        binding.skipButton.setOnClickListener {
+            findNavController().navigate(R.id.action_resultFragment_to_FirstFragment)
+        }
+
+        binding.edittext.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val imm = v.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, 0)
+                true
+            } else {
+                false
+            }
+        }
+
+
+
 
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.konfettiView.start(party)
+    }
 
     fun sendScore(name: String, score: Int) {
         val rank = Rank(name, score)
