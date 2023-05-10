f73a7b142fb1ca6bad83dc33b4721152a87d27fa
unknown <augereau.jeremy@protonmail.com>
2023-05-10 18:53:48 +0200

    confetti ok

--- app/src/main/java/com/example/triviup/ResultFragment.kt
+++ app/src/main/java/com/example/triviup/ResultFragment.kt
@@ -5,29 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.triviup.databinding.FragmentResultBinding
 import com.example.triviup.model.Rank
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import nl.dionsegijn.konfetti.core.Party
+import nl.dionsegijn.konfetti.core.Position
+import nl.dionsegijn.konfetti.core.emitter.Emitter
+import java.util.concurrent.TimeUnit
 
 
 class ResultFragment : Fragment() {
 
     private lateinit var databaseReference: DatabaseReference
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        databaseReference = Firebase.database.reference
-    }
+    private var _binding: FragmentResultBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var party : Party
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+        databaseReference = Firebase.database.reference
+        _binding = FragmentResultBinding.inflate(inflater)
+
+        party = Party(
+            speed = 0f,
+            maxSpeed = 30f,
+            damping = 0.9f,
+            spread = 360,
+            colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+            emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(100),
+            position = Position.Relative(0.5, 0.15)
+        )
+
+        binding.submitButton.setOnClickListener {
+            binding.konfettiView.start(party)
+        }
+
+        return binding.root
     }
+
 
     fun sendScore(name: String, score: Int) {
         val rank = Rank(name, score)
